08c605e03335e163c7912942a8db40a4288426cd
resteve <resteve@zikzakmedia.com>
2014-10-06 13:28:31 +0200

    Change str DISPLAY_MSG

--- sale.py
+++ sale.py
@@ -7,7 +7,7 @@ from flask.ext.paginate import Pagination
 
 sale = Blueprint('sale', __name__, template_folder='templates')
 
-DISPLAY_MSG = lazy_gettext('Displaying <b>{start} - {end}</b> {record_name} of <b>{total}</b>')
+DISPLAY_MSG = lazy_gettext('Displaying <b>{start} - {end}</b> of <b>{total}</b>')
 
 SHOPS = current_app.config.get('TRYTON_SALE_SHOPS')
 LIMIT = current_app.config.get('TRYTON_PAGINATION_SALE_LIMIT', 20)
